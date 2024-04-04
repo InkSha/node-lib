7c844d727fb14e734573b58cec0e49a1b691625d
InkSha <x1a010@163.com>
2024-04-04 17:21:30 +0800

    chore(config): init project

---
+++ .eslintrc.cjs
@@ -0,0 +1,37 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+    '@typescript-eslint/no-explicit-any': [2, { ignoreRestArgs: true }],
+  },
+}
---
+++ src/index.ts
@@ -0,0 +1,42 @@
+/**
+ * @package node-lib
+ * @author InkSha
+ * @license MIT
+ * @description Some Develop Library.
+ * @create 2024-04-04
+ * @update 2024-04-04
+ */
+
+/**
+ * package name
+ */
+export const PACKAGE_NAME = 'node-lib'
+/**
+ * package version name
+ * version release pacakge name
+ */
+export const PACKAGE_VERSION = 'alpha'
+/**
+ * package master version number
+ * update new feature
+ */
+export const MASTER_VERSION = 0
+/**
+ * package second version number
+ * change current feature
+ */
+export const SECOND_VERSION = 0
+/**
+ * package patch version number
+ * fix current question
+ */
+export const PATCH_VERSION = 0
+/**
+ * package correct version number
+ * optimize current feature
+ */
+export const CORRECT_VERSION = 0
+/**
+ * package current version
+ */
+export const CURRENT_VERSION = `${PACKAGE_VERSION} ${MASTER_VERSION}.${SECOND_VERSION}.${PATCH_VERSION}.${CORRECT_VERSION}`
